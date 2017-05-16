0db02d7772507b1aa3af7f42f34fff0bfe74ed18
Aleksey Paschenko <paschenko.aleks@gmail.com>
2017-05-16 12:35:43 +0300

    added CRUD methods to cards controller, added some views

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,4 @@
 class CardsController < ApplicationController
-
   def index
     @cards = Card.all
   end
@@ -7,4 +6,47 @@ class CardsController < ApplicationController
   def show
     @card = Card.find(params[:id])
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+    if @card.save
+      redirect_to @card, notice: 'Карточка успешно создана'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @card.update(params)
+      redirect_to @card, notice: "Карточка обновлена"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @card.destroy
+  end
+
+  private
+
+    def card_params
+      params.require(:card).permit(:original_text, :translated_text)
+    end
 end
+
+
+
+
+
+
+
+
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,11 @@
 class Card < ApplicationRecord
+  validates :original_text, :translated_text, presence: true
+  #validates :original_text, :translated_text, presence: true
+
+  before_create :set_review_date
+
+  private
+    def set_review_date
+      @params = Time.now + (60*60*24*3)
+    end
 end
