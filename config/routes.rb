b9a06262901d8c1cb42851459b86d87aff7010e1
Aleksey Paschenko <paschenko.aleks@gmail.com>
2017-05-16 10:18:08 +0300

    second task

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'cards/index'
+
+  resources :cards
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
