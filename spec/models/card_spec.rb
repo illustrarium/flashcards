908aa1db9f0f33c821d63074352c658b7284ec73
Aleksey Paschenko <paschenko.aleks@gmail.com>
2017-05-19 15:43:06 +0300

    spec modified

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Card, :type => :model do
-  before(:each) do
-    @card = FactoryGirl.create(:card)
-  end
+  let(:card) { FactoryGirl.create(:card) }
 
   it "Check translate" do
-    expect(@card.check_translate("battlestart")).to be false
+    expect(card.check_translate("battlestart")).to be false
   end
 
   it "Set review date 3 days from now" do
-    expect(@card.set_review_date.strftime('%d/%m/%Y')).to eq(3.days.from_now.strftime('%d/%m/%Y'))
+    expect(card.set_review_date.strftime('%d/%m/%Y')).to eq(3.days.from_now.strftime('%d/%m/%Y'))
   end
 end
