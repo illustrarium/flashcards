56b9d59762a5311230591a9ddb39a446373c4c71
Aleksey Paschenko <paschenko.aleks@gmail.com>
2017-05-22 12:40:57 +0300

    hound ci comments

--- spec/feature/check_translation_spec.rb
+++ spec/feature/check_translation_spec.rb
@@ -1,6 +1,6 @@
 # require 'spec_helper.rb'
 require 'rails_helper.rb'
- 
+
 feature 'Checkin translation' do 
   before :each do
     @card = FactoryGirl.create(:card)
@@ -9,32 +9,13 @@ feature 'Checkin translation' do
   end
 
   scenario 'Failed' do
-    # 1 Идем на главную
+    # 1. Goes to "/"
     visit '/'
-    # 2. Вводим неверный вариант
+    # 2. Wrong iput
     fill_in 'user_variant', with: 'Believer'
-    # 3. Кликаем проверить
+    # 3. Click check btn
     click_button 'Проверить'
-    # 4. Ожидаем страницу с контентом "неверно"
+    # 4. Expect "Wrong" notice
     expect(page).to have_content('Неверно')
   end
 end
-
-# describe "Checkin translation", :type => :feature do
-#   before :each do
-#     @card = FactoryGirl.create(:card)
-#     @card.review_date = 2.days.ago
-#     @card.save
-#   end
-
-#   it "Failed" do
-#     # 1 Идем на главную
-#     visit root_path
-#     # 2. Вводим неверный вариант
-#     fill_in 'user_variant', with: 'Believer'
-#     # 3. Кликаем проверить
-#     click_link 'Проверить'
-#     # 4. Ожидаем страницу с контентом "неверно"
-#     expect(page).to have_content('Неверно')
-#   end
-# end
