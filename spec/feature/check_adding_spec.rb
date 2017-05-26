5a06e5a1174bf6bad1db7a5a223beeaca7b1079b
Aleksey Paschenko <paschenko.aleks@gmail.com>
2017-05-26 16:22:36 +0300

    added deck factory to test

--- spec/feature/check_adding_spec.rb
+++ spec/feature/check_adding_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper.rb'
 
 feature 'Check adding card' do
   before :each do
+    @deck = FactoryGirl.create(:deck)
     @user = FactoryGirl.create(:user)
     login_user_post("cat2@cat.ru", "123") # login in with parameters
   end
