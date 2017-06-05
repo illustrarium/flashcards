8ffaf7bacdb6a3da55f771c6e250fa2c1515575a
Aleksey Paschenko <paschenko.aleks@gmail.com>
2017-06-05 16:53:58 +0300

    adited spec

--- spec/feature/check_adding_spec.rb
+++ spec/feature/check_adding_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper.rb'
 
 feature 'Check adding card' do
   before :each do
-    @deck = FactoryGirl.create(:deck)
     @user = FactoryGirl.create(:user)
+    @deck = FactoryGirl.create(:deck, user_id: @user.id)
     login_user_post("cat2@cat.ru", "123") # login in with parameters
   end
 
