645fd74f44505ef8f39afd97b5ab33d8ae44ef42
Aleksey Paschenko <paschenko.aleks@gmail.com>
2017-06-09 10:35:06 +0300

    hound comments

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,11 +1,11 @@
 function setScoreVal(score) {
-  $('#time').val(score)
+  $('#time').val(score);
 }
 function setScore()  {
   var time = 1000;
   var score = 5;
   for(i = 0; i < 6; i++ ) {
-    setTimeout(setScoreVal, time, score)
+    setTimeout(setScoreVal, time, score);
     time = time + 20000;
     score--;
   }
